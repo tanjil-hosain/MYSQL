652030abfeae86753733c549648e85cf2c6f143a
tanjil-hosain <tanjilh85@gmail.com>
2026-04-26 11:02:14 +0600

    insert form

--- Coffee_shop/db.connection.php
+++ Coffee_shop/db.connection.php
@@ -7,8 +7,36 @@ $connect = mysqli_connect("localhost","root", "", "coffee_shop" );
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
+    <style>
+        form{
+            width: 400px;
+        }
+    </style>
 </head>
 <body>
+   <fieldset>
+     <form>
+  <div class="mb-3">
+    <label class="form-label">name</label>
+    <input type="text" class="form-control" name="name">
+    
+  </div>
+   <div class="mb-3">
+    <label class="form-label">name</label>
+    <input type="text" class="form-control" name="name">
+    
+  </div>
+    <div class="mb-3">
+    <label class="form-label">name</label>
+    <input type="text" class="form-control" name="name">
+    
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+   </fieldset>
+    <br>
+
         <table border="1">
     <thead>
         <tr>
@@ -33,5 +61,6 @@ $connect = mysqli_connect("localhost","root", "", "coffee_shop" );
         ?>
     </tbody>
 </table> 
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
