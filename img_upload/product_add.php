9fa131ab0c9988398b1398b076a51caba3150178
tanjil-hosain <tanjilh85@gmail.com>
2026-04-29 11:10:35 +0600

    add Products from

--- img_upload/product_add.php
+++ img_upload/product_add.php
@@ -1,3 +1,18 @@
+<?php
+$database = mysqli_connect("localhost", "root", "" , "img_upload");
+?>
+<?php
+if(isset($_POST['brand'])){
+    $name= $_POST['name'];
+    $contact= $_POST['contact'];
+    $sql= "INSERT INTO brands(name, contact) VALUES('$name', '$contact')";
+
+    if(mysqli_query($database, $sql)){
+        header("location: product_add.php");
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,6 +27,24 @@
         Contact: <br>
         <input type="text" name="contact"> <br> <br>
         <input type="submit" name="brand" value="Add Brand">
+    </form>
+
+    <form action="" method="post">
+        Name: <br>
+        <input type="text" name="name"> <br>
+        Price: <br>
+        <input type="text" name="price"> <br> <br>
+        Brand_id: <br>
+        <select name="brand_id" >
+            <?php 
+            $brand_data = $database->query("SELECT * FROM brands");
+            while(list($id,$brand_name) = $brand_data->fetch_assoc()){
+                echo "<option value = '$id'> $brand_name</option>";
+            }
+             ?>
+        </select>
+        
+        Image: 
     </form>
 </body>
 </html>
